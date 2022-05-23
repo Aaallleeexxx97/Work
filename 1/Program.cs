ebc6eebcfe51cff8328fade90b9f7ea953257999
AlexsandrFK <sasha.furik.241@gmail.com>
2022-05-23 12:44:56 +1200

    аписал метод ввода

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+          
+public class Program
+{
+    public static void Main()
+  {
+  } 
+     public static string[] input()
+  {
+    Console.WriteLine("Введите количество элементов массива");
+    int leng = Convert.ToInt32(Console.ReadLine());
+    string[] mass = new string[leng];
+    for(int i = 0;i<leng;i++)
+    {
+      Console.WriteLine("введите "+(i+1)+" элемент массива");
+      string temp = Convert.ToString(Console.ReadLine());
+      mass[i] = temp;
+    }
+    return mass;
+  }
+}
+
+
